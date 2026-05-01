a626e019294321a0b2970aef198f250ce163cfb1
Viraj Rajguru <virajrajguru67@gmail.com>
2026-05-02 02:08:40 +0530

    fix: simplify CORS for production resilience

--- server/index.js
+++ server/index.js
@@ -11,19 +11,7 @@ const googleClient = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const app = express();
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow local dev and your specific Vercel domain
-        const allowedOrigins = [
-            'http://localhost:5173',
-            'http://localhost:3000',
-            'https://script-mind-psi.vercel.app'
-        ];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1 || origin.endsWith('.vercel.app')) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true, // This will reflect the request origin, which is safe with credentials
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Action-Type']
